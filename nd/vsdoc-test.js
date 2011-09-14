100c0b64b0727e41c9984608fc309b8700c1aada
kinogam <kinogam@gmail.com>
2011-09-14 22:31:58 +0800

    add globle params

---
+++ nd/vsdoc-test.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="node-vsdoc.js" />
+
+
+var hello = require('../hello')
+
+/*
+http
+  .cat('http://localhost:3000/')
+  .addCallback(function (data) {
+      callbackFired = true;
+      assert.equal('hello world', data);
+      hello.server.close();
+  });
+
+process.addListener('exit', function () {
+    assert.ok(callbackFired);
+});
+*/
